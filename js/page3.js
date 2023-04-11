3f4bc853f61d7f12d5964842e0594aef61125d35
TMikkiNT <kozieli1997@gmail.com>
2023-04-11 15:39:44 +0300

    улучшенная версия задании
    Всё исправлено для удобства в просмотре

--- js/page3.js
+++ js/page3.js
@@ -1,5 +1,3 @@
-//Дана строка. Необходимо вывести в консоль перевёрнутый вариант. Например, "Hello" -> "olleH".
-
 function reverseString(str) {
     return str.split("").reverse().join("");  
   } 
--- js/page6.js
+++ js/page6.js
@@ -1,16 +1,12 @@
-/*Дан массив. Проверить, одинаковые ли элементы в массиве и вывести результат true или false в консоль. Речь идёт не о двух рядом стоящих 
-одинаковых элементах, а обо всех. Проверить, все ли элементы в массиве одинаковые.*/
-
-const arr = [5, 15, 34, 37, 44, 122, 358, 999, 37, 88, 11];
+const arr = [5, 15, 34, 37, 44, 122, 358, 999, 35, 88, 11];
 let uniq = [];
-function hasDuplicates(arr) {
-  return new Set(arr).size !== arr.length;
-}
 
-for (let i = 0; i < arr.length; i++){
+function hasDuplicates(arr) {
+  return arr.every(val => val === arr[0]);
+}
 
 if (hasDuplicates(arr)) {
   console.log("Duplicate elements found.");
 } else {
   console.log("No Duplicates found.");
-}}
+}
